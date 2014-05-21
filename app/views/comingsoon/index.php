5e86a1140ecdc6c726dd500709fc8040b5657566
Green <andy199310@gmail.com>
2014-05-21 14:48:04 +0800

    adding coming soon

---
+++ app/views/comingsoon/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>大一生活知訊網</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+			background-color: rgb(83, 83, 83);
+		}
+		#coming_soon{
+			margin: 0 auto;
+			width: 1000px;
+			height: 1000px;
+		}
+
+
+	</style>
+</head>
+	<body>
+		<div id="coming_soon">
+			<img src="images/coming_soon.png">
+		</div>
+	</body>
+</html>
