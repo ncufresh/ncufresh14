c78d935b9498a0cf39b2c2acf846998ee3f7489d
Green <andy199310@gmail.com>
2014-06-05 17:39:15 +0800

    Add jquery and bootstrap

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -1,9 +1,18 @@
 <html>
 	<head>
-
+		{{ HTML::script('js/jquery.min.js') }}
+		{{ HTML::script('js/bootstrap.min.js') }}
+		{{ HTML::style('css/bootstrap.css') }}
 	</head>
 	<body>
-		@include('layouts.auth')
-		@yield('content')
+		<div id="top">
+			@include('layouts.top')
+		</div>
+		<div id="container">
+			@yield('content')
+		</div>
+		<div id="bottom">
+
+		</div>
 	</body>
 </html>
---
+++ app/views/layouts/top.blade.php
@@ -0,0 +1,2 @@
+
+@include('layouts.auth')
