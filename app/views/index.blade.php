a3e6826982198b7f7dab6d9cf3bc3ebfcc84eab6
InIn <inin610719@gmail.com>
2014-06-06 21:54:32 +0800

    edit index style. add index.css

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.layout')
-
+{{ HTML::style('css/index.css') }}
 @section('content')
-@if(Auth::check())
-	Hello! {{ Auth::user()->name}}
-@else
-	No login!QQ
-@endif
-<br>
-HIHI;
+<div id="contentContainer" class="testR">
+	<div id="contentLeft" class="testB">
+		<div id="calender" class="testG">calender
+		</div>
+		<div id="links" class="testY">links
+		</div>
+	</div>
+	<div id="contentRight" class="testB">
+		<div id="board" class="testG">board
+		</div>
+		<div id="forum" class="testY">forum
+		</div>
+		<div id="vedio" class="testR">vedio
+		</div>
+	</div>
+</div>
 
 @stop
