a508760e562dc31fa63b1f51a46708b720b70ba6
Green <andy199310@gmail.com>
2014-06-09 23:07:56 +0800

    modified model

--- app/models/Announcement.php
+++ app/models/Announcement.php
@@ -3,6 +3,17 @@
  * User: green
  * Date: 2014/6/3
  * Time: 23:18
+ *
+ * @property integer $id
+ * @property integer $user_id
+ * @property string $title
+ * @property string $content
+ * @property integer $viewer
+ * @property boolean $pinned
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property \Carbon\Carbon $deleted_at
+ * @property-read \User $user
  */
 
 
--- app/models/Links.php
+++ app/models/Links.php
@@ -1,6 +1,15 @@
 <?php
 
 
+/**
+ * Links
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
 class Links extends Eloquent{
 
 	protected $table = 'links';
