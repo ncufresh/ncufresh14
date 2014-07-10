241e11ee95be90fb7b7115b2a05ad69b641aa7d1
InIn <inin610719@gmail.com>
2014-07-11 01:02:42 +0800

    fix problem if no user in database.(just in snake)

--- app/controllers/GameSnakeController.php
+++ app/controllers/GameSnakeController.php
@@ -6,7 +6,16 @@ class GameSnakeController extends BaseController
 	public function index()
 	{
 		$user = Game::find(1);
-		$name = User::where('id', '=', 1)->firstOrFail();
+		if ( !$user ) {
+			$user = new Game;
+			$user->power = 5;
+			$user->gp = 0;
+			$name = new User;
+			$name->nick_name = "訪客";
+		}
+		else {
+			$name = User::where('id', '=', 1)->firstOrFail();	
+		}	
 		return View::make('game.snake', array('user' => $user, 'name' => $name));
 	}
 
