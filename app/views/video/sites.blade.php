6c168a0cbd2efbee9aba302fa08f43b96a75ae42
yo-he,Wang <s9003041223@yahoo.com.tw>
2014-07-14 22:52:23 +0800

    finished, beautify code tomorrow

--- app/views/video/sites.blade.php
+++ app/views/video/sites.blade.php
@@ -12,26 +12,17 @@
 	<iframe width="560" height="315" src="//www.youtube.com/embed/mzX0rhF8buo" frameborder="0" allowfullscreen></iframe>
 </div>
 
-	@if(isset($introduction) )
-<div id = "intro">{{$introduction}}</div> <!--video introduction-->
-	@endif
+@if(isset($introduction) )
+	<div id = "intro">{{$introduction}}</div> <!--video introduction-->
+@endif
 
-
-<img src="images\youtube縮圖\deargod.jpg" id="attached1" class="ath1">
-<img src="images\youtube縮圖\紀念.jpg" id="attached2" class="ath2">
-<img src="images\youtube縮圖\夜空中最亮的星.jpg" id="attached3" class="ath3">
-<img src="images\youtube縮圖\山丘.jpg" id="attached4" class="ath4">
-<img src="images\youtube縮圖\李白.jpg" id="attached5" class="ath">
-
-<!---->
-<div id="video_name" style ="width:900px;height:100px;font-size:80px;">影片名稱</div>
+<div id="video_name" style ="width:500px;height:100px;font-size:80px;">影片名稱</div>
 <button id="like" type="button" style="width:100px;height:30px;"><img src="圖片網址"></button>
 <button id="love" type="button" style="width:100px;height:30px;"><img src="圖片網址"></button>
 <button id="message" style="width:100px;height:30px;">留言</button>
 <div id="div1" type="submit"  style="width:900px; height:150px; display:none;">
-
 	{{ Form::open(array('route' => 'video.message')) }}
-	{{ Form::textarea('video_text') }}
+	{{ Form::textarea('video_text','',array('maxlength'=>380)) }}
 	{{ Form::submit('送出') }}
 	{{ Form::close() }}
 </div>
@@ -43,16 +34,16 @@
 <div id="rating_number2" style="text-align:center ; font-size:27px">{{$getLove}}</div>
 
 @foreach( $messages as $message)
-<div id="user_message">
-	<a href="https://www.youtube.com/watch?v=ibWYROwadYs"><img src="images\youtube縮圖\頭貼.jpg" id="portrait"></a>
-	<div id= "div_message" style="width:450px; height:150px;">{{$message["video_text"]}}</div>
-</div>
+	<div id="user_message">
+		<a href="https://www.youtube.com/watch?v=ibWYROwadYs"><img src="images\youtube縮圖\頭貼.jpg" id="portrait"></a>
+		<div id= "div_message" style="width:820px; height:150px;">{{$message["video_text"]}}</div>
+	</div>
 @endforeach
-</div>
-	@if($errors -> any())
+</div> <!--frame div-->
 
-<script>
-	{{" alert('".$errors -> first()."');"}}
-</script>
-	@endif
+{{$messages->links();}} 
+@if($errors -> any())
+	<script>{{"alert('".$errors -> first()."');"}}</script>
+@endif
+
 @stop
--- public/js/video.js
+++ public/js/video.js
@@ -1,4 +1,5 @@
 $(function(){	
+  /*
   $(".ath1").click(function(){
   $("#main").empty();
   video_id = 1;
@@ -6,10 +7,14 @@ $(function(){
   $('<iframe width="560" height="315" src="//www.youtube.com/embed/mzX0rhF8buo" frameborder="0" allowfullscreen></iframe>').appendTo($('#main'));
   
     var url = getTransferData('about_rate_url');
-    var data = {video_id: video_id , id: id};
+    var data = {video_id: video_id };
 
       ajaxPost(url, data , rateNumber);
 
+    var url = getTransferData('change_intro_url');
+    var intro = {id : id};
+
+      ajaxGet(url, intro , changeIntro);
   });
 
   $(".ath2").click(function(){
@@ -19,9 +24,14 @@ $(function(){
   $('<iframe id="i_attached1" width="560" height="315" src="//www.youtube.com/embed/ePFWiPo0FzE" frameborder="0" allowfullscreen></iframe>').appendTo($('#main'));
   
     var url = getTransferData('about_rate_url');
-    var data = {video_id: video_id , id : id};
+    var data = {video_id: video_id};
 
       ajaxPost(url, data , rateNumber);
+
+    var url = getTransferData('change_intro_url');
+    var intro = {id : id};
+
+      ajaxGet(url, intro , changeIntro);
 });
 
   $(".ath3").click(function(){
@@ -31,9 +41,14 @@ $(function(){
   $('<iframe id="i_attached2" width="560" height="315" src="//www.youtube.com/embed/fzuy63eCUKc" frameborder="0" allowfullscreen></iframe>').appendTo($('#main'));
     
     var url = getTransferData('about_rate_url');
-    var data = {video_id: video_id , id : id};
+    var data = {video_id: video_id};
 
       ajaxPost(url, data , rateNumber);
+    var url = getTransferData('change_intro_url');
+    var intro = {id : id};
+
+      ajaxGet(url, intro , changeIntro);
+
   });
 
   $(".ath4").click(function(){
@@ -43,9 +58,13 @@ $(function(){
   $('<iframe id="i_attached3" width="560" height="315" src="//www.youtube.com/embed/rVEMTxg_LrU" frameborder="0" allowfullscreen></iframe>').appendTo($('#main'));
   
     var url = getTransferData('about_rate_url');
-    var data = {video_id: video_id , id:id};
+    var data = {video_id: video_id };
 
       ajaxPost(url, data , rateNumber);
+    var url = getTransferData('change_intro_url');
+    var intro = {id : id};
+
+      ajaxGet(url, intro , changeIntro);
   });
 
   $(".ath5").click(function(){
@@ -55,57 +74,61 @@ $(function(){
   $('<iframe id="i_attached4" width="560" height="315" src="//www.youtube.com/embed/2xKc-rAyAdQ" frameborder="0" allowfullscreen></iframe>').appendTo($('#main'));
     
     var url = getTransferData('about_rate_url');
-    var data = {video_id: video_id , id : id};
+    var data = {video_id: video_id };
 
       ajaxPost(url, data , rateNumber);
-  });
+    var url = getTransferData('change_intro_url');
+    var intro = {id : id};
 
+      ajaxGet(url, intro , changeIntro);
+  });
+*/
   $("#message").click(function(){
-  $("#div1").fadeToggle("slow");
-  });
+    $("#div1").fadeToggle("slow");
+  }); //fade 留言框
 
- var video_id=1;
+ //var video_id=1;
 
   $('#like').click(function(){   
 		var url = getTransferData('like_video_url');
-    var data = {video_id: video_id, video_rate: 0};
+    var data = {video_id: 1, video_rate: 0};
 
-	  	ajaxPost(url, data , rateFinish);
-	});
+	  ajaxPost(url, data , rateFinish);
+	});  //like button
 
   $('#love').click(function(){
   	var url = getTransferData('like_video_url');
-	  var data = {video_id: video_id, video_rate: 1};
+	  var data = {video_id: 1, video_rate: 1};
 
-  	   ajaxPost(url, data , rateFinish);
-	});
+    ajaxPost(url, data , rateFinish);
+	});  //love button
 
-
+/*
   function shit(){
     var url = getTransferData('like_video_url');
     var data = {video_id: video_id};
 
       ajaxPost(url, data , 0);
-  }
-
+  }   //determine which video
+*/
 
   function rateFinish(gg){
-    window.location.reload();
-  }
+    $("#rating_number1").text(gg['a']);
+    $("#rating_number2").text(gg['b']);
+  }   //vote完重整，讓rating 即時顯示
 
   function LogInAlert(alert){
     alert("請先登入!");
-  }
-/*
-    var url = getTransferData('about_introduction_url');
-    var data = { id : id};
+  }   //訪客留言
 
-      ajaxPost(url, data , 0);
-*/
   function rateNumber(data){
     $('#rating_number1').text(data["videoRating1"]);
     $('#rating_number2').text(data["videoRating2"]);
     //$('about_introduction_url').text(data["id"]);
+  }   //顯示rating data
+/*
+  function changeIntro(intro){
+    $('#intro').text(intro["video_introduction"]);
   }
-
+*/
 });
