ad3a49306b3288afe5549c91005ce6adb63ddf40
InIn <inin610719@gmail.com>
2014-07-14 16:07:09 +0800

    add migrate.

--- app/views/game/power.blade.php
+++ app/views/game/power.blade.php
@@ -1,11 +1,11 @@
 @extends('game.layout')
 
 @section('js_css')
-	{{ HTML::style('css/game/game.css') }}
+	{{ HTML::style('css/game.css') }}
+	{{ HTML::style('css/game.css') }}
 	{{ HTML::script('js/game/power.js') }}
 @stop
 
 @section('game_content')
 
 @stop
-
