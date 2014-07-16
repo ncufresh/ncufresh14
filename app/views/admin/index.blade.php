f10a3fb222eb803469e67910523b7a1a99e135e6
Green <andy199310@gmail.com>
2014-07-17 03:02:09 +0800

    Add back to dashboard

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -10,8 +10,11 @@
 		@if(Auth::user()->can('manage_editor'))
 			<div class="functions-block">
 				<h3>編輯文案</h3>
+				<a href="{{ route('nculife.data') }}"><button id="admin-link">中大生活文章管理</button></a>
+				<a href="{{ route('necessity.necessity_backstage_freshman') }}"><button id="admin-link">新生必讀(大學部)文章管理</button></a>
+				<a href="{{ route('necessity.necessity_backstage_research') }}"><button id="admin-link">新生必讀(研究生)文章管理</button></a>
+				<a href="{{ route('necessity.necessity_backstage_download') }}"><button id="admin-link">新生必讀(檔案區)文章管理</button></a>
 				<a href="{{ route('SchoolGuide.list') }}"><button id="admin-link">校園導覽文章管理</button></a>
-
 			</div>
 		@endif
 {{-- manage_link manage_announcement manage_calender --}}
