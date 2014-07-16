bbb729e36693a103ba074a011a09b5b70022504d
InIn <inin610719@gmail.com>
2014-07-16 20:30:49 +0800

    fix .

--- app/views/layouts/auth.blade.php
+++ app/views/layouts/auth.blade.php
@@ -5,21 +5,6 @@
 		<a href="{{ route('logout') }}">Logout</a>
 	</div>
 @else
-
-
-{{-- User not logined. show login form --}}
-	{{-- Login form --}}
-	{{ Form::open(array('route' => 'login', 'id' => 'login-form')) }}
-	{{-- Form::label('email', '帳號') --}}
-	{{ Form::text('email', null, array('id' => 'account', 'placeholder' => '帳號')) }}
-	<br>
-	{{-- Form::label('密碼') --}}
-	{{ Form::password('password', array('id' => 'password', 'placeholder' => '密碼')) }}
-	<br>
-	<a href="{{ route('register') }}"><div id="register"></div></a>
-	{{ Form::submit('', array('id' => 'submit')) }}
-	<a href="{{ route('login.FB') }}"><div id="fblogin"></div></a>
-	{{ Form::close() }}
 	
 	{{-- User not logined. show login form --}}
 	<div id="auth_nolgin">
