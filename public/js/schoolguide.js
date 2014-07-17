8701159562b59a1114cd65d73ab701de51d3c910
kate1011good <kate1011good@gmail.com>
2014-07-17 17:26:19 +0800

    burl

--- public/js/schoolguide.js
+++ public/js/schoolguide.js
@@ -30,8 +30,8 @@ $(document).ready(function(){
 			var name =data[i]['name'];
 			var id=data[i]['id'];
 			var category = data[i]['categories'];
-			$("<img class ='line' src='burl+"/images/SchoolGuide/line.png"'").appendTo("#leftlist").data('place_id', id);
-			$("<img class ='board' src='burl+"/images/SchoolGuide/board.png"'").appendTo("#leftlist").data('place_id', id);
+			$("<img class ='line' src='"+burl+"/images/SchoolGuide/line.png'").appendTo("#leftlist").data('place_id', id);
+			$("<img class ='board' src='"+burl+"/images/SchoolGuide/board.png'").appendTo("#leftlist").data('place_id', id);
 			$("<li class='left_item'>"+name+"</li>").appendTo("#leftlist").data('place_id', id);
 
 			if(category==1){
