f2ef951e7cb018f57c7f6d82ac1a9cbeed21bc36
Green <andy199310@gmail.com>
2014-07-21 16:16:48 +0800

    Fix user not found

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -6,7 +6,11 @@ class UserController extends BaseController{
 		App::make('SiteMap')->pushLocation('個人專區', route('user.id', array('id' => $id)));
 		$user = User::find($id);
 		$talk = UserTalk::all()->random(1);
-		return View::make('user.index', array('user' => $user, 'talk' => $talk));
+		if($user == NULL){
+			return Redirect::to('/')->with('alert-message', '查無此人');
+		}else{
+			return View::make('user.index', array('user' => $user, 'talk' => $talk));
+		}
 	}
 
 	public function selfIndex(){
