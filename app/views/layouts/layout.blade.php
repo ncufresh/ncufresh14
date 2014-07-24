214147cafbe5cf073482c9cbb803a8fad0962574
Green <andy199310@gmail.com>
2014-07-24 13:30:09 +0800

    Make layout minify

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -22,19 +22,9 @@
 		
 		{{ HTML::script('js/layout/robot.js') }}
 
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-			@if(Request::server('SERVER_IP') == '140.115.184.136' && Config::get('app.debug') == false)
-
-				ga('create', 'UA-10121863-1', 'auto');
-				ga('require', 'displayfeatures');
-				ga('send', 'pageview');
-			@endif
-		</script>
+		@if(Request::server('SERVER_IP') == '140.115.184.136' && Config::get('app.debug') == false)
+			{{ HTML::script('js/ga.js') }}
+		@endif
 
 		@yield('js_css')
 
