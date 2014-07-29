d83329c8bec63876c6c2cfef23f6810a672df4b8
Green <andy199310@gmail.com>
2014-07-29 16:35:26 +0800

    More go

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
 		<meta property="og:title" content="2014大一生活知訊網">
 		<meta property="og:type" content="website">
 		<meta property="og:url" content="http://ncufresh14.weigreen.com/">
-		<meta property="og:image" content="http://ncufresh14.weigreen.com/images/banner.png" />
+		<meta property="og:image" content="http://ncufresh14.weigreen.com/images/banner-big.png" />
 		<meta property="og:image:type" content="image/png" />
 		<meta property="og:site_name" content="2014大一生活知訊網" />
 		<meta property="og:description" content="對未來的大學生活充滿期待嗎? 或是對中大有著什麼疑問呢? 那就別再考慮了 快來新鮮人專屬的 大一生活知訊網看看吧!" />
