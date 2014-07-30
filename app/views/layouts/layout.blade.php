74a57a04d17e476b3432aed4f1866eb4abd16ea6
InIn <inin610719@gmail.com>
2014-07-30 13:08:34 +0800

    add konami code.

--- app/views/layouts/layout.blade.php
+++ app/views/layouts/layout.blade.php
@@ -30,6 +30,8 @@
 		
 		{{ HTML::script('js/layout/robot.js') }}
 
+		{{ HTML::script('js/ininKonami.js') }}
+
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
--- public/js/main.js
+++ public/js/main.js
@@ -170,3 +170,35 @@ $(function(){
 
 	}
 });
+
+$.konami = function(options)
+{
+    var options = $.extend({
+        code:                   [38, 38, 40, 40, 37, 39, 37, 39],
+        interval:               10,
+        complete:               function()
+        {
+           alert("you complete a konami code");
+        }
+    }, options);
+    var index = 0;
+    var interval = options.interval;
+    var timer = setInterval(function()
+    {
+        if ( interval-- <= 0 ) index = 0;
+    }, 50);
+    $(document).keyup(function(event)
+    {
+        if (
+            event.keyCode != 231
+         && event.keyCode == options.code[index]
+        )
+        {
+            interval = options.interval;
+            if ( index++ == options.code.length - 1 ) options.complete();
+            return true;
+        }
+        index = 0;
+        return true;
+    });
+};
