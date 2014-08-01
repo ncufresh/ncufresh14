a8319617d3d9cbf98ec4d4e73a6fd68a7259b7b9
Green <andy199310@gmail.com>
2014-08-02 00:12:46 +0800

    error mail back

--- app/views/emails/error.blade.php
+++ app/views/emails/error.blade.php
@@ -2,6 +2,6 @@
 <body>
 <h5>Time: {{ $now }}</h5>
 <h3>Title: {{-- $exception->getMessage() --}}</h3>
-<h4>Error: {{  nl2br($exception->gettraceasstring()) }}</h4>
+<h4>Error: {{  nl2br($exception->getTraceAsString()) }}</h4>
 </body>
 </html>
