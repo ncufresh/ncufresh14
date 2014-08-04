d3433b15856ec910176fca9dc50822b2f88805aa
InIn <inin610719@gmail.com>
2014-08-04 18:41:59 +0800

    gmae campus images add done.

--- app/views/game/campus.blade.php
+++ app/views/game/campus.blade.php
@@ -20,27 +20,27 @@
 
 		<div id="gameCampusMap">
 			<div id="gameCampusGameBox" action="{{ URL::to('game/campus/check') }}">
-				<img id="gameCampusMapBack" src="{{ asset('images/SchoolGuide/Buttom.png') }}">
+				<div id="gameCampusMapBack"></div>
 				<div id="gameCampusLifeBox">
 					<div class="gameCampusLife"></div>
 					<div class="gameCampusLife"></div>
 					<div class="gameCampusLife"></div>
 				</div>
 				<div class="gameCampusType" id="gameCampusType0"><!--系館-->
-					<div class="gameCampusBuilding" id="science_5" index="0"></div>
+					<div class="gameCampusBuilding" id="science-5" index="0"></div>
 					<div class="gameCampusBuilding" id="literary" index="1"></div>
-					<div class="gameCampusBuilding" id="engineer_1" index="2"></div>
-					<div class="gameCampusBuilding" id="engineer_2" index="3"></div>
-					<div class="gameCampusBuilding" id="engineer_3" index="4"></div>
-					<div class="gameCampusBuilding" id="engineer_4" index="5"></div>
-					<div class="gameCampusBuilding" id="engineer_5" index="6"></div>
-					<div class="gameCampusBuilding" id="science_4" index="7"></div>
-					<div class="gameCampusBuilding" id="science_3" index="8"></div>
-					<div class="gameCampusBuilding" id="science_2" index="9"></div>
-					<div class="gameCampusBuilding" id="science_1" index="10"></div>
+					<div class="gameCampusBuilding" id="engineer-1" index="2"></div>
+					<div class="gameCampusBuilding" id="engineer-2" index="3"></div>
+					<div class="gameCampusBuilding" id="engineer-3" index="4"></div>
+					<div class="gameCampusBuilding" id="engineer-4" index="5"></div>
+					<div class="gameCampusBuilding" id="engineer-5" index="6"></div>
+					<div class="gameCampusBuilding" id="science-4" index="7"></div>
+					<div class="gameCampusBuilding" id="science-3" index="8"></div>
+					<div class="gameCampusBuilding" id="science-2" index="9"></div>
+					<div class="gameCampusBuilding" id="science-1" index="10"></div>
 					<div class="gameCampusBuilding" id="math" index="11"></div>
-					<div class="gameCampusBuilding" id="management_1" index="12"></div>
-					<div class="gameCampusBuilding" id="management_2" index="13"></div>
+					<div class="gameCampusBuilding" id="management-1" index="12"></div>
+					<div class="gameCampusBuilding" id="management-2" index="13"></div>
 					<div class="gameCampusBuilding" id="Hakkas" index="14"></div>
 					<div class="gameCampusBuilding" id="electric" index="15"></div>
 				</div>
@@ -59,7 +59,7 @@
 					<div class="gameCampusBuilding" id="ncu" index="11"></div>
 				</div>
 				<div class="gameCampusType" id="gameCampusType2"><!--中大十景-->
-					<div class="gameCampusBuilding" id="G14_ground" index="0"></div>
+					<div class="gameCampusBuilding" id="G14-ground" index="0"></div>
 					<div class="gameCampusBuilding" id="door" index="1"></div>
 					<div class="gameCampusBuilding" id="monument" index="2"></div>
 					<div class="gameCampusBuilding" id="tree" index="3"></div>
@@ -67,6 +67,8 @@
 					<div class="gameCampusBuilding" id="park" index="5"></div>
 					<div class="gameCampusBuilding" id="lake" index="6"></div>
 					<div class="gameCampusBuilding" id="flower" index="7"></div>
+					<!--add-->
+					<div class="gameCampusBuilding" id="pool" index="8"></div>
 				</div>
 				<div class="gameCampusType" id="gameCampusType3"><!--運動-->
 					<div class="gameCampusBuilding" id="gym" index="0"></div>
@@ -77,9 +79,16 @@
 					<div class="gameCampusBuilding" id="skate" index="5"></div>
 					<div class="gameCampusBuilding" id="valley" index="6"></div>
 					<div class="gameCampusBuilding" id="badminton" index="7"></div>
+					<!--add-->
+					<div class="gameCampusBuilding" id="tennis" index="8" ></div>
 				</div>
 				<div class="gameCampusType" id="gameCampusType4"><!--飲食-->
 					<div class="gameCampusBuilding" id="wood" index="0"></div>
+					<!--addd-->
+					<div class="gameCampusBuilding" id="B7-food" index="1"></div>
+					<div class="gameCampusBuilding" id="park-food" index="2"></div>
+					<div class="gameCampusBuilding" id="Graduate-food" index="3"></div>
+					<div class="gameCampusBuilding" id="B9-food" index="4"></div>
 				</div>
 				<div class="gameCampusType" id="gameCampusType5"><!--住宿-->
 					<div class="gameCampusBuilding" id="B11" index="0"></div>
@@ -90,10 +99,12 @@
 					<div class="gameCampusBuilding" id="International" index="5"></div>
 					<div class="gameCampusBuilding" id="B3" index="6"></div>
 					<div class="gameCampusBuilding" id="G14" index="7"></div>
-					<div class="gameCampusBuilding" id="G_1_4" index="8"></div>
+					<div class="gameCampusBuilding" id="G-1-4" index="8"></div>
 					<div class="gameCampusBuilding" id="B9" index="9"></div>
 					<div class="gameCampusBuilding" id="B12" index="10"></div>
 					<div class="gameCampusBuilding" id="Graduate" index="11"></div>
+					<!--add-->
+					<div class="gameCampusBuilding" id="G5" index="12"></div>
 				</div>
 				<div id="gameCampusQuestion"></div>
 			</div>
